3e23f979aeb7500589fa2598dc443215fccd91ce
Rumon <crash.msr@gmail.com>
2020-02-25 18:32:44 +0600

    added: 'area-of-triangle' code

---
+++ src/area-of-triangle/README.md
@@ -0,0 +1,6 @@
+# Instructions
+
+1. Take 3 numbers from the user. These numbers will represent the sides of the triangle.
+2. Calculate the area of that triangle and display it to the user.
+
+> Note: You may need to check if the numbers user provided form a valid triangle.
---
+++ src/area-of-triangle/areaTri.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int isValidTriangle(int a, int b, int c)
+{
+  // Theory: https://en.wikipedia.org/wiki/Triangle_inequality
+  if (a + b <= c || b + c <= a || c + a <= b)
+    return 0;
+
+  return 1;
+}
+
+void main()
+{
+  int edge1, edge2, edge3;
+
+  printf("Enter edge-1: ");
+  scanf("%d", &edge1);
+
+  printf("Enter edge-2: ");
+  scanf("%d", &edge2);
+
+  printf("Enter edge-3: ");
+  scanf("%d", &edge3);
+
+  if (isValidTriangle(edge1, edge2, edge3) == 1)
+  {
+    int perimeter = edge1 + edge2 + edge3;
+    float s = (float)perimeter / 2; // casting 'perimeter' into float to prevent this from returning int
+    printf("\nArea: %0.2f sq. unit", sqrt(s * (s - edge1) * (s - edge2) * (s - edge3)));
+  }
+  else
+    printf("The edges will not form any valid triangle!");
+}
