40c0aba49157d8cff3d6e38d91910b4ec8d9db84
Rumon <crash.msr@gmail.com>
2020-02-26 15:17:16 +0600

    included 'stdbool' library

--- lib/triangle.h
+++ lib/triangle.h
@@ -1,8 +1,10 @@
+#include <stdbool.h>
+
 int isValidTriangle(int a, int b, int c)
 {
   // Theory: https://en.wikipedia.org/wiki/Triangle_inequality
   if (a + b <= c || b + c <= a || c + a <= b)
-    return 0;
+    return false;
 
-  return 1;
+  return true;
 }
