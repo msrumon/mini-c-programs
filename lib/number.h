82bf55690556fe1e5e5310ee60c163d11ae3b4fd
Rumon <crash.msr@gmail.com>
2020-02-27 14:00:17 +0600

    created 'findLCM' function

--- lib/number.h
+++ lib/number.h
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 int findGCD(int number1, int number2)
 {
   // Theory: https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclid's_algorithm
@@ -5,4 +7,10 @@ int findGCD(int number1, int number2)
     return number2;
 
   return findGCD(number2 % number1, number1);
+}
+
+long findLCM(int number1, int number2)
+{
+  // Theory: https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
+  return abs(number1 * number2) / findGCD(number1, number2);
 }
