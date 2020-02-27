e215c04e40d8cc14cf254ba5e2d8d4f6454c01bb
Rumon <crash.msr@gmail.com>
2020-02-27 12:52:50 +0600

    changed types to 'unsigned' integers

--- src/area-of-triangle/areaTri.c
+++ src/area-of-triangle/areaTri.c
@@ -4,7 +4,7 @@
 
 void main()
 {
-  int edge1, edge2, edge3;
+  unsigned int edge1, edge2, edge3;
 
   printf("Enter edge-1: ");
   scanf("%d", &edge1);
@@ -15,9 +15,9 @@ void main()
   printf("Enter edge-3: ");
   scanf("%d", &edge3);
 
-  if (isValidTriangle(edge1, edge2, edge3) == 1)
+  if (isValidTriangle(edge1, edge2, edge3))
   {
-    int perimeter = edge1 + edge2 + edge3;
+    unsigned int perimeter = edge1 + edge2 + edge3;
     float s = (float)perimeter / 2; // casting 'perimeter' into float to prevent the calculation from returning integer
     printf("Area: %0.2f sq. unit", sqrt(s * (s - edge1) * (s - edge2) * (s - edge3)));
   }
