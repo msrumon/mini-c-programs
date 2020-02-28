aa218e58b9f851fef1a1d055e118ba890bcd9186
Rumon <crash.msr@gmail.com>
2020-02-28 14:23:12 +0600

    added some additional functions

--- lib/number.h
+++ lib/number.h
@@ -1,6 +1,42 @@
 #include <stdlib.h>
 
-int findGCD(int number1, int number2)
+int greaterBetween(const int number1, const int number2)
+{
+  if (number1 > number2)
+    return number1;
+
+  return number2;
+}
+
+int greatestAmong(int *numbers, const int size)
+{
+  int greatest = *(numbers + 0);
+
+  for (int i = 1; i < size; i++)
+    greatest = greaterBetween(greatest, *(numbers + i));
+
+  return greatest;
+}
+
+int smallerBetween(const int number1, const int number2)
+{
+  if (number1 > number2)
+    return number2;
+
+  return number1;
+}
+
+int smallestAmong(int *numbers, const int size)
+{
+  int smallest = *(numbers + 0);
+
+  for (int i = 1; i < size; i++)
+    smallest = smallerBetween(smallest, *(numbers + i));
+
+  return smallest;
+}
+
+int findGCD(const int number1, const int number2)
 {
   // Theory: https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclid's_algorithm
   if (number1 == 0)
@@ -9,7 +45,7 @@ int findGCD(int number1, int number2)
   return findGCD(number2 % number1, number1);
 }
 
-long findLCM(int number1, int number2)
+long findLCM(const int number1, const int number2)
 {
   // Theory: https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
   return abs(number1 * number2) / findGCD(number1, number2);
