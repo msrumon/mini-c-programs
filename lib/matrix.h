e2b833e219d9599254aa0388ae9e093e453d26f8
Rumon <crash.msr@gmail.com>
2020-02-29 15:47:30 +0600

    worked around #1

--- lib/matrix.h
+++ lib/matrix.h
@@ -1,40 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void vanish(int **matrix, const unsigned row)
+void display(const char *message, const size_t row, const size_t column, int matrix[row][column])
 {
-  for (int i = 0; i < row; i++)
-    free(matrix + i);
-
-  free(matrix);
-}
-
-int **take(const unsigned row, const unsigned column, const char *message)
-{
-  printf("\n%s \n", message);
-
-  int **matrix = (int **)calloc(row, sizeof(int *));
-
-  for (int a = 0; a < column; a++)
-    *(matrix + a) = (int *)calloc(column, sizeof(int));
-
-  /* scanf */
-  for (int r = 0; r < row; r++)
-  {
-    for (int c = 0; c < column; c++)
-    {
-      printf("a%d%d: ", r + 1, c + 1);
-      scanf("%d", ((matrix + r) + c));
-    }
-  }
-  /* endscanf */
-
-  return matrix;
-}
-
-void spit(int **matrix, const unsigned row, const unsigned column, const char *message)
-{
-  printf("\n%s \n", message);
+  printf("\n%s\n", message);
 
   for (int r = 0; r < row; r++)
   {
@@ -43,32 +11,39 @@ void spit(int **matrix, const unsigned row, const unsigned column, const char *m
     for (int c = 0; c < column; c++)
     {
       if (c == column - 1)
-        printf("%d", *((matrix + r) + c));
+        printf("%d", matrix[r][c]);
       else
-        printf("%d\t", *((matrix + r) + c));
+        printf("%d\t", matrix[r][c]);
     }
 
     printf("|\n");
   }
 }
 
-// int **multiply(int **matrix1, int **matrix2, const unsigned row1, const unsigned column2, const unsigned multiplier)
-// {
-//   int **multiplied = (int **)calloc(row1, sizeof(int *));
+void manipulate(const char *message, const size_t row, const size_t column, int matrix[row][column])
+{
+  printf("\n%s\n", message);
 
-//   for (int a = 0; a < column2; a++)
-//     *(multiplied + a) = (int *)calloc(column2, sizeof(int));
+  for (int r = 0; r < row; r++)
+  {
+    for (int c = 0; c < column; c++)
+    {
+      printf("a%d%d: ", r + 1, c + 1);
+      scanf("%d", &matrix[r][c]);
+    }
+  }
+}
 
-//   for (int r = 0; r < row1; r++)
-//   {
-//     for (int c = 0; c < column2; c++)
-//     {
-//       *((multiplied + r) + c) = 0;
+void multiply(const size_t row1, const size_t column1, const size_t row2, const size_t column2, const int matrix1[row1][column1], const int matrix2[row2][column2], int result[row1][column2])
+{
+  for (int r = 0; r < row1; r++)
+  {
+    for (int c = 0; c < column2; c++)
+    {
+      result[r][c] = 0;
 
-//       for (int m = 0; m < multiplier; m++)
-//         *((multiplied + r) + c) += *((matrix1 + r) + m) * *((matrix2 + m) + c);
-//     }
-//   }
-
-//   return multiplied;
-// }
+      for (int m = 0; m < row2 || m < column1; m++)
+        result[r][c] += matrix1[r][m] * matrix2[m][c];
+    }
+  }
+}
